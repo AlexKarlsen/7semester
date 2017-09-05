a8f9eac0868d61c75fc95ac4168fd87e914f0885
Alex Karlsen <alexkarlsen7@gmail.com>
2017-09-05 14:18:23 +0200

    engines spec

--- Mandatory Assignment 1/app.js
+++ Mandatory Assignment 1/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var
 
 const index = require('./app_server/routes/index');
 const users = require('./app_server/routes/users');

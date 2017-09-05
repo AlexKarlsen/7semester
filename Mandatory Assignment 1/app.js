8bd3ec555491be342ba87aff1accd5670d7cec83
Alex Karlsen <alexkarlsen7@gmail.com>
2017-09-05 15:01:24 +0200

    removed var

--- Mandatory Assignment 1/app.js
+++ Mandatory Assignment 1/app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var
 
 const index = require('./app_server/routes/index');
 const users = require('./app_server/routes/users');

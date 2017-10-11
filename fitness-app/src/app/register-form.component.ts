e0181f6133acc32074e53cdfd1b3bcc07cfe27a0
Emil <emil-ny@hotmail.com>
2017-10-11 14:36:54 +0200

    Separated auth into own file. Works this time

--- fitness-app/src/app/register-form.component.ts
+++ fitness-app/src/app/register-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse } from '@angular/common/http';
 import { User } from './user';
+import { Authentication } from './authentication';
 
 @Component({
     selector: 'register-form',
@@ -25,53 +25,4 @@ export class RegisterFormComponent{
             this.auth.register(this.model);
             console.log('post request sent');
         }
-}
-
-interface AuthResponse {
-    token : string;
-}
-
-export class Authentication
-{
-    model = new User();
-
-    private saveToken(token:string) {
-        window.localStorage['JW-token'] = token;
-    }
-    private getToken(token: string) {
-        if(window.localStorage['JW-token']) {
-            return window.localStorage['JW-token'];
-        } 
-        else
-        {
-            return '';
-        }
-    }
-
-    constructor(private http: HttpClient) {}
-
-    public register(user: User) {
-        //const url = `https://peaceful-temple-74079.herokuapp.com/auth/register`;
-        const url = 'http://localhost:3000/auth/register';
-
-        this.http.post<AuthResponse>(url, user).subscribe(data => {
-            console.log('Something good happened');
-            this.saveToken(data.token);
-            return true;
-        },
-        // Errors will call this callback instead:
-        (err: HttpErrorResponse) => {
-            if(err.error instanceof Error) {
-                // A client - side or network error occurred. Handle it accordingly.
-                console.log('An error occurred:', err.error.message);
-            } 
-            else
-            {
-                // The backend returned an unsuccessful response code.
-                // The response body may contain clues as to what went wrong,
-                console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-            }
-            return false;
-        });
-    }
 }

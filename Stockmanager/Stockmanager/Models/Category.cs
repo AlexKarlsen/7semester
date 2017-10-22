a8c42ed8184b3e0a8fa78f9b5e196d59d8749b48
Alex Justesen Karlsen <63664@grundfos.com>
2017-10-22 21:26:27 +0200

    Initial commit and model classes

---
+++ Stockmanager/Stockmanager/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stockmanager.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            ComponentTypes = new List<ComponentType>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<ComponentType> ComponentTypes { get; protected set; }
+
+    }
+}
